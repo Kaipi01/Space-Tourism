1eb28da1c2523102e2430687152fff04307e09ec
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2022-12-11 21:56:25 +0100

    Create a menu toggle scripte and import and initialize swiper

--- src/script.js
+++ src/script.js
@@ -1,19 +1,61 @@
-const menuSwitchBtn = document.querySelector(".nav__menu-btn");
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
-menuSwitchBtn.addEventListener("click", () => {
-  const menu = document.querySelector(".nav__menu");
-  //const menuLinks = document.querySelectorAll(".nav__menu-link");
+const menuSwitchBtn = document.querySelector(".nav__menu-btn");
+const destinationNavBtns = document.querySelectorAll(".destination__nav-btn");
+const menu = document.querySelector(".nav__menu");
+const menuLinks = document.querySelectorAll(".nav__menu-link");
+const swiper = new Swiper(".swiper", {
+  loop: false,
+  speed: 1000,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+removeAddMenuTabindex();
+
+menuSwitchBtn.addEventListener("click", openCloseMenu);
+
+destinationNavBtns.forEach((btn) => {
+  setFirstSlideBtnActive(btn);
+
+  btn.addEventListener("click", () => {
+    changeSlide(btn);
+  });
+});
+
+function openCloseMenu() {
   const menuSwitchBtnIcons = document.querySelectorAll(".nav__menu-btn-icon");
-  
   menu.classList.toggle("nav__menu--open");
-  menuSwitchBtnIcons.forEach(icon => {
+  menuSwitchBtnIcons.forEach((icon) => {
     icon.classList.toggle("nav__menu-btn-icon--hide");
   });
-//   menuLinks.forEach(link => {
-//     if (menu.classList.contains('nav__menu--open')) {
-//         link.removeAttribute('tabindex', '-1');
-//     } else {
-//         link.setAttribute('tabindex', '-1');
-//     }
-//   });
-});
+
+  removeAddMenuTabindex();
+}
+
+function changeSlide(btn) {
+  destinationNavBtns.forEach((btn) =>
+    btn.classList.remove("destination__nav-btn--active")
+  );
+  swiper.slideTo(btn.dataset.slide, 1000);
+  btn.classList.add("destination__nav-btn--active");
+}
+
+function setFirstSlideBtnActive(btn) {
+  if (btn.dataset.slide === "0") {
+    btn.classList.add("destination__nav-btn--active");
+  }
+}
+
+function removeAddMenuTabindex() {
+  menuLinks.forEach((link) => {
+    if (menu.classList.contains("nav__menu--open")) {
+      link.removeAttribute("tabindex", "-1");
+    } else {
+      link.setAttribute("tabindex", "-1");
+    }
+  });
+}
